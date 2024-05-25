e605bc7da62cee09d99856658547aa71fd34dafb
Michael Nguyen <26033112+16-Bits@users.noreply.github.com>
2024-05-24 20:22:48 -0700

    Prototype cleanup

--- test image.py
+++ test image.py
@@ -1,82 +1,50 @@
 import cv2 as cv
 import numpy as np
-from matplotlib import pyplot as plt
 
-def debugShowImage(img):
+def debugShowImage(img):#debug function to show image
     cv.imshow('DEBUG', img)
     cv.waitKey(0)
     cv.destroyAllWindows()
 
-'''
-contour iamge
-contour parchment
-step through image contours, match to parchment
-find diemensions of parchment, crop image
-'''
-def main():
-    #preprocess input image
-    inputimgPath = r"C:\Users\blast\Desktop\TC4-Solver\example research\2024-05-24_14.23.30.png"
-    origScreenshot = cv.imread(inputimgPath)
-    inputImage = cv.imread(inputimgPath, cv.IMREAD_GRAYSCALE)
-    assert inputImage is not None, "file could not be read, check with os.path.exists()"
-    ret, inputThresh = cv.threshold(inputImage,100,255,cv.THRESH_BINARY)
-    #debugShowImage(inputImage)
-    #preprocess minigame area parchment
-    pcmnt = cv.imread(r'C:\Users\blast\Desktop\TC4-Solver\images\parchment.png', cv.IMREAD_GRAYSCALE)
-    ret, parchThresh = cv.threshold(pcmnt,100,255,cv.THRESH_BINARY)
-
-    contours, hierarchy = cv.findContours(parchThresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
-    parchCont = contours[0]
-    '''
-    parchContImage = cv.drawContours(parchThresh, parchCont, -1, (100,255,100), 3)
-    debugShowImage(parchContImage)
-    '''
-
+def cropMinigame(inputImg):
+    #preprocess input screenshot image
+    inputColor = inputImg.copy()
+    inputGray = cv.cvtColor(inputColor, cv.COLOR_BGR2GRAY)
+    ret, inputThresh = cv.threshold(inputGray,100,255,cv.THRESH_BINARY)
+    #get contours of input screenshot, remove small contours
     contours, hierarchy = cv.findContours(inputThresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
-
-    #debugShowImage(resultImg)
-
-    #512 arbitrary area
-    filteredContours = list(filter(lambda c: cv.contourArea(c) > 512, contours))
-
-    minigameCont2 = min(filteredContours, key=lambda c: cv.matchShapes(parchCont, c, 3,0))
-    #print(cv.matchShapes(parchCont, minigameCont2, 3,0))
-    newIMG = cv.drawContours(origScreenshot.copy(), minigameCont2, -1, (0,255,0), 3)
+    filteredContours = list(filter(lambda c: cv.contourArea(c) > 512, contours)) #512 area is arbitrary
+    #preprocess minigame area parchment
+    minigameImg = cv.imread(r'images\parchment.png', cv.IMREAD_GRAYSCALE)
+    assert minigameImg is not None, "file could not be read, check with os.path.exists()"
+    ret, minigameThresh = cv.threshold(minigameImg,100,255,cv.THRESH_BINARY)
+    #get contours of minigame area
+    contours, hierarchy = cv.findContours(minigameThresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+    minigameSourceCont = contours[0]
+    #find minigame contour in input image
+    minigameInputCont = min(filteredContours, key=lambda c: cv.matchShapes(minigameSourceCont, c, 1,0))
+    '''
+    #visuals of minigame area contours and bounding box
+    newIMG = cv.drawContours(inputColor.copy(), minigameInputCont, -1, (0,255,0), 3)
     newIMG = cv.drawContours(newIMG.copy(), filteredContours, -1, (255,0,0), 3)
-    #debugShowImage(newIMG)
-    rect = cv.minAreaRect(minigameCont2)
-    bound = cv.boxPoints(rect)
-
-    bound = np.intp(bound)
-    x, y, w, h = bound[3,0], bound[1,1], bound[1,0], bound[3,1]
+    debugShowImage(newIMG)
     newIMG = cv.drawContours(newIMG.copy(), [bound], -1, (0,0,255), 3)
     debugShowImage(newIMG)
-    cropImg = newIMG[y:h, x:w]
+    '''
+    #get bounding box of minigame contour
+    rect = cv.minAreaRect(minigameInputCont)
+    bound = np.intp(cv.boxPoints(rect))
+    #crop input image based on bounding box info
+    x, y, w, h = bound[3,0], bound[1,1], bound[1,0], bound[3,1]
+    cropImg = inputColor[y:h, x:w]
+    return cropImg
+
+def main():
+    inputImgPath = r"example research\2024-05-24_14.23.30.png"
+    screenshotImg = cv.imread(inputImgPath)
+    assert screenshotImg is not None, "file could not be read, check with os.path.exists()"
+    cropImg = cropMinigame(screenshotImg)
     debugShowImage(cropImg)
-
-'''
-contour iamge
-contour parchment
-step through image contours, match to parchment
-find diemensions of parchment, crop image
-
-
-
-
-#blur = cv.GaussianBlur(img,(5,5),0)
-#debugShowImage(blur)
-
-#debugShowImage(binThresh)
-
-
-
-contours, hierarchy = cv.findContours(binThresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
-minigameArea = max(contours, key=cv.contourArea)
-contImg = cv.drawContours(img, minigameArea, -1, (255,255,255), 3)
-#debugShowImage(contImg)
-ret = cv.matchShapes(parchCont, minigameArea, 1, 0.0)
-print(ret)
-'''
 
 if __name__ == "__main__":
     main()

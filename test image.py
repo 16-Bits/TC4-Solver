6e7a2d85ade16f1fff99bd8ba82dbea4b5955d3e
Michael Nguyen <26033112+16-Bits@users.noreply.github.com>
2024-05-29 18:42:22 -0700

    Added debug parameter and fixed minigame zone detection

--- test image.py
+++ test image.py
@@ -1,6 +1,8 @@
 import cv2 as cv
 import numpy as np
 
+debug = False
+
 
 def debugShowImage(img):  # debug function to show image
     cv.imshow('DEBUG', img)
@@ -17,7 +19,7 @@ def cropMinigame(inputImg):
     contours, hierarchy = cv.findContours(
         inputThresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
     filteredContours = list(filter(lambda c: cv.contourArea(
-        c) > 512, contours))  # 512 area is arbitrary
+        c) > 1024, contours))  # 1024 area is arbitrary
     # preprocess minigame area parchment
     minigameImg = cv.imread(r'images\parchment.png', cv.IMREAD_GRAYSCALE)
     assert minigameImg is not None, "file could not be read, check with os.path.exists()"
@@ -29,17 +31,20 @@ def cropMinigame(inputImg):
     # find minigame contour in input image
     minigameInputCont = min(
         filteredContours, key=lambda c: cv.matchShapes(minigameSourceCont, c, 1, 0))
-    '''
-    #visuals of minigame area contours and bounding box
-    newIMG = cv.drawContours(inputColor.copy(), minigameInputCont, -1, (0,255,0), 3)
-    newIMG = cv.drawContours(newIMG.copy(), filteredContours, -1, (255,0,0), 3)
-    debugShowImage(newIMG)
-    newIMG = cv.drawContours(newIMG.copy(), [bound], -1, (0,0,255), 3)
-    debugShowImage(newIMG)
-    '''
     # get bounding box of minigame contour
     rect = cv.minAreaRect(minigameInputCont)
     bound = np.intp(cv.boxPoints(rect))
+
+    if debug is True:
+        # debug visuals of minigame area contours and bounding box
+        newIMG = cv.drawContours(
+            inputColor.copy(), minigameInputCont, -1, (0, 255, 0), 3)
+        newIMG = cv.drawContours(
+            newIMG.copy(), filteredContours, -1, (255, 0, 0), 3)
+        debugShowImage(newIMG)
+        newIMG = cv.drawContours(newIMG.copy(), [bound], -1, (0, 0, 255), 3)
+        debugShowImage(newIMG)
+
     # crop input image based on bounding box info
     x, y, w, h = bound[3, 0], bound[1, 1], bound[1, 0], bound[3, 1]
     cropImg = inputColor[y:h, x:w]
@@ -47,7 +52,7 @@ def cropMinigame(inputImg):
 
 
 def main():
-    inputImgPath = r"example research\2024-05-24_14.23.30.png"
+    inputImgPath = r"example research\2024-05-15_19.09.52.png"
     screenshotImg = cv.imread(inputImgPath)
     assert screenshotImg is not None, "file could not be read, check with os.path.exists()"
     cropImg = cropMinigame(screenshotImg)
